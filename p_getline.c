1e0d9c392d78f85437027ae97a2af471f3de4cb1
Primotion Studio <88973898+PrimotionStudio@users.noreply.github.com>
2023-11-09 10:41:46 +0100

    Getline documentation

--- p_getline.c
+++ p_getline.c
@@ -4,51 +4,57 @@ static char buffer[BUFFER_SIZE];
 static size_t buffer_index = 0;
 static size_t chars_read = 0;
 
-char *custom_getline(void) {
+/**
+ * custom_getline - Used in place of getline
+ */
+char *custom_getline(void)
+{
     size_t i = 0;
     char *line = NULL;
     int newline_found = 0;
 
-    while (1) {
-        if (buffer_index >= chars_read) {
+    while (1)
+    {
+        if (buffer_index >= chars_read)
+        {
             buffer_index = 0;
             chars_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            if (chars_read == 0) {
-                if (line) {
+            if (chars_read == 0)
+            {
+                if (line)
                     free(line);
-                }
-                return NULL;  /* End of file */
+                return NULL;
             }
-            else if (chars_read == (size_t)-1) {
+            else if (chars_read == (size_t)-1)
+            {
                 perror("read");
-                if (line) {
+                if (line)
                     free(line);
-                }
-                exit(EXIT_FAILURE);  /* Error reading from input */
+                exit(EXIT_FAILURE);
             }
         }
-
-        for (; buffer_index < chars_read; ++buffer_index) {
-            if (buffer[buffer_index] == '\n') {
+        for (; buffer_index < chars_read; ++buffer_index)
+        {
+            if (buffer[buffer_index] == '\n')
+            {
                 newline_found = 1;
                 buffer_index++;
                 break;
             }
             i++;
         }
-
-        /* Allocate memory for the line and copy characters */
-        if (newline_found) {
+        if (newline_found)
+        {
             line = malloc(i + 1);
-            if (line == NULL) {
+            if (line == NULL)
+            {
                 perror("malloc");
-                exit(EXIT_FAILURE);  /* Allocation error */
+                exit(EXIT_FAILURE);
             }
             strncpy(line, buffer + buffer_index - i - 1, i);
             line[i] = '\0';
             break;
         }
     }
-
     return line;
 }
